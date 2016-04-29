c6fae36f2260234e9b50c75f83e7614c589fa114
Ori Klein <ori@Oris-MacBook-Pro.local>
2016-04-28 21:04:42 -0700

    Initial Commit

---
+++ ECSelectionSort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ECSelectionSort
+//
+//  Created by Ori Klein on 4/28/16.
+//  Copyright (c) 2016 Ori Klein. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
