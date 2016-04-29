c1aee6134a6eab31312f34dafc97cdc3e0fda959
oriklein195 <ori@geshem.com>
2016-04-28 21:34:41 -0700

    Add everything

--- ECSelectionSort/main.cpp
+++ ECSelectionSort/main.cpp
@@ -1,15 +1,50 @@
-//
-//  main.cpp
-//  ECSelectionSort
-//
-//  Created by Ori Klein on 4/28/16.
-//  Copyright (c) 2016 Ori Klein. All rights reserved.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+class SelectionSort{
+public:
+    
+    void print(int *arr){
+        for (int i = 0; i < 8; i++){
+            cout << *(arr + i) << ", ";
+        }
+        cout << endl;
+    }
+
+    int indexOfSmallest(int arr[], int size, int index){
+        if (index == size - 1)
+                return index;
+        int smallestInNext = indexOfSmallest(arr, size, index+1);
+        if (arr[smallestInNext] < arr[index])
+            return smallestInNext;
+        else
+            return index;
+    }
+    
+    int* sort(int arr[], int size, int index){
+        if (index == size - 1){
+            return arr;
+        }
+        int small = indexOfSmallest(arr, size, index);
+        int temp = arr[index];
+        arr[index] = arr[small];
+        arr[small] = temp;
+        arr = sort(arr, size, index + 1);
+        return arr;
+    }
+};
+
+int main(){
+    SelectionSort s = SelectionSort();
+    int arr[] = { 5, 6, 8, 2, 4, 10, 35, 1 };
+    int *intptr = arr;
+    intptr = s.sort(arr, 8, 0);
+    
+    s.print(intptr);
+    
+    system("pause");
     return 0;
+    
 }
+
